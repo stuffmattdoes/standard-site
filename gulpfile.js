1c9be0a8d47393d352774eadce132a44f2f51e4b
Matthew J. Morrison <m.james.morrison00@gmail.com>
2016-05-13 23:37:29 -0400

    - Fixed gulpfile - Added directories to the images

--- gulpfile.js
+++ gulpfile.js
@@ -20,18 +20,18 @@ var gulp            = require('gulp'),
 
 // Plugins
 var 
-    chalk       = require('chalk'),
+    // chalk       = require('chalk'),
     concat      = require('gulp-concat'),
-    gutil       = require('gulp-util'),
-    imageMin    = require('gulp-imagemin'),
-    inquirer    = require('inquirer'),
+    // gutil       = require('gulp-util'),
+    imagemin    = require('gulp-imagemin'),
+    // inquirer    = require('inquirer'),
     livereload  = require('gulp-livereload'),
     maps        = require('gulp-sourcemaps'),
-    notifier    = require('node-notifier'),
+    // notifier    = require('node-notifier'),
     rename      = require('gulp-rename');
-    rsync       = require('rsyncwrapper').rsync,
+    // rsync       = require('rsyncwrapper').rsync,
     sass        = require('gulp-sass'),
-    uglify      = require('gulp-uglify'),    
+    uglify      = require('gulp-uglify')
 ;
 
 
@@ -40,53 +40,50 @@ var
 // -------------------------------------------------
 
 // Default - watch for any changes
-gulp.task('default', function() {
+gulp.task('default', ['styles', 'scripts', 'images'], function() {
     gulp.watch(devPath + '/scss/**/*.scss', ['styles']);
     gulp.watch(devPath + '/js/**/*.js', ['scripts']);
-    gulp.watch(distPath + '/img', ['images']);
+    gulp.watch(distPath + '/img/original/**', ['images']);
 });
 
 gulp.task('styles', function() {
-    return gulp.src(devPath + 'sass/main.scss')
+    return gulp.src(devPath + '/scss/main.scss')
         .pipe(maps.init())
-
-        // // Create an unminified CSS file first
-        // .pipe(sass({
-        //     errLogToConsole: true,
-        //     outputStyle: 'expanded'
-        // }))
-        // .pipe(gulp.dest('css/'))
 
         // Now create some minified CSS
         .pipe(sass({
             errLogToConsole: true,
             outputStyle: 'compressed'
         }))
-        .pipe(maps.write(devPath + "/css"))
 
         // Rename it so we don't overwrite our unmin CSS
         .pipe(rename({
+            basename: 'main',
             suffix: '.min'
         }))
-        .pipe(gulp.dest('css/'));
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest(distPath + '/css/'))
+        .pipe(livereload());
 });
 
 gulp.task('scripts', function () {
-    return gulp.src(scriptsArray)
+    return gulp.src(devPath + '/js/**/*.js')
         .pipe(maps.init())
         .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(maps.write(assetsPath))
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest(deistPath + '/js'));
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest(distPath + '/js/'))
+        .pipe(livereload());
 });
 
 gulp.task('images', function() {
-    gulp.src(distPath + '/img')
-    .pipe(imagemin)
-    .pipe(gulp.dest(distPath + '/img'))
+    gulp.src(distPath + '/img/original/**')
+    .pipe(imagemin())
+    .pipe(gulp.dest(distPath + '/img/min/'))
+    .pipe(livereload());
 });
 
 // -------------------------------------------------
@@ -94,39 +91,39 @@ gulp.task('images', function() {
 // -------------------------------------------------
 
 // Deploy to server
-gulp.task('deploystaging', function(callback) {
-    inquirer.prompt({
-        type: "confirm",
-        name: 'deployConfirm',
-        message: "You are about to deploy to dev. Are you sure you want to do this?",
-        default: false
-      }, function(answers) {
-        if(answers.deployConfirm){
-            rsync({
-                ssh: true,
-                src: distPath,
-                dest: 'path/to/destination',
-                recursive: true,
-                deleteAll: true,
-                args: ['--verbose', '--compress', '--archive'],
-                dryRun: false,
-                exclude: ignoreList
-            },
-            function(error, stdout, stderr, cmd) {
-                if(error){
-                    gutil.log('error: ', chalk.red(error));
-                    notifier.notify({ message: 'rsync error' });
-                    callback();
-                }else{
-                    gutil.log('rsync result: ','\n', chalk.gray(stdout));
-                    notifier.notify({ message: 'rsync complete' });
-                    callback();
-                }
-            });
-        }else{
-            gutil.log('that was close!');
-            callback();
-        }
+// gulp.task('deploystaging', function(callback) {
+//     inquirer.prompt({
+//         type: "confirm",
+//         name: 'deployConfirm',
+//         message: "You are about to deploy to dev. Are you sure you want to do this?",
+//         default: false
+//       }, function(answers) {
+//         if(answers.deployConfirm){
+//             rsync({
+//                 ssh: true,
+//                 src: distPath,
+//                 dest: 'path/to/destination',
+//                 recursive: true,
+//                 deleteAll: true,
+//                 args: ['--verbose', '--compress', '--archive'],
+//                 dryRun: false,
+//                 exclude: ignoreList
+//             },
+//             function(error, stdout, stderr, cmd) {
+//                 if(error){
+//                     gutil.log('error: ', chalk.red(error));
+//                     notifier.notify({ message: 'rsync error' });
+//                     callback();
+//                 }else{
+//                     gutil.log('rsync result: ','\n', chalk.gray(stdout));
+//                     notifier.notify({ message: 'rsync complete' });
+//                     callback();
+//                 }
+//             });
+//         }else{
+//             gutil.log('that was close!');
+//             callback();
+//         }
 
-    });
-});
+//     });
+// });
